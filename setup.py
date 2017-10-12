212e3ea851f150ae152b7108f90fec12c1684515
maxmunzel <maxmunzel@me.com>
2017-10-12 13:21:39 +0200

    Restructured into an installable python package
    TODO:
    * differentiate between browsers
    * optimize query frequency
    * implement some kind of validation
    * Achievements
    * Admin interface
    * Work around double bookings on reload / with multiple tabs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="strichliste",
+    packages=["strichliste"],
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "flask-sqlalchemy", 'eventlet'
+    ]
+)

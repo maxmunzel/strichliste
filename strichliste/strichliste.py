bc9258d8a696cb052f3646759708ef4ad56f4b51
maxmunzel <maxmunzel@me.com>
2017-10-17 11:07:18 +0200

    Some improvements to Testing Mode Testing mode now always resets at start and uses the sqlite inmemory db. (regardless of flags)     TODO:         * add periodical reloads         * add api to change prices         * optimize query frequency         * Achievements         * Admin interface         * Work around double bookings on reload / with multiple tabs

--- strichliste/strichliste.py
+++ strichliste/strichliste.py
@@ -59,10 +59,15 @@ HOST = args.host                                    # default: "0.0.0.0"
 SQLALCHEMY_DATABASE_URI = args.db                   # default: "sqlite:////tmp/test.db"
 PSK = args.psk                                      # default: ""
 
+# always use a in-memory db, when testing to avoid data loss due to human error
+if TESTING:
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    print("connected to: " + SQLALCHEMY_DATABASE_URI)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
@@ -310,6 +315,6 @@ def send_static(path):
 
 
 if __name__ == "__main__":
-    if RESET:
+    if RESET or TESTING:
         init_with_dummy_data()
     current_app.run(debug=DEBUG, host=HOST, port=PORT)
